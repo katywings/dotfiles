8c381f0c904a1997da7f72881abbcb9f83211ddd
Katja Lutz <mail@katjalutz.ch>
2016-11-03 21:59:19 +0100

    Added z bin and powerline configs for fish and tmux

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,2 +1,8 @@
 source "$HOME/.homesick/repos/homeshick/completions/homeshick.fish"
 . "$HOME/.homesick/repos/homeshick/homeshick.fish"
+
+# Modify the terminal's 256 color palette to use the gruvbox theme
+set -l GRUVBOX_SCRIPT ~/.dein-vim/repos/github.com/morhetz/gruvbox/gruvbox_256palette.sh
+if test -f $GRUVBOX_SCRIPT
+  bash $GRUVBOX_SCRIPT
+end
